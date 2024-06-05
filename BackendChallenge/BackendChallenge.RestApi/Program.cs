52d82955f4d628c9001804ad17b4458e5960ae6e
lmarroquindev <luismarroquindev@gmail.com>
2024-06-04 21:00:32 -0600

    Dbcontext inyected by dependency in program of api project

--- BackendChallenge/BackendChallenge.RestApi/Program.cs
+++ BackendChallenge/BackendChallenge.RestApi/Program.cs
@@ -1,6 +1,15 @@
+using BackendChallenge.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ChallengeVueNetContext>(options =>
+{
+   options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+   options.EnableSensitiveDataLogging();
+}, ServiceLifetime.Scoped);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
