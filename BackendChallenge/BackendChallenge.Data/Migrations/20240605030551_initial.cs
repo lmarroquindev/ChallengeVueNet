6dccc517161c1d2c44cafd13b37bf0bdd4c98235
lmarroquindev <luismarroquindev@gmail.com>
2024-06-04 21:09:53 -0600

    Initial Migration created

---
+++ BackendChallenge/BackendChallenge.Data/Migrations/20240605030551_initial.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackendChallenge.Data.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.CreateTable(
+                name: "PermissionType",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PermissionType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Permission",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    PermissionTypeId = table.Column<int>(type: "int", nullable: false),
+                    PermissionDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Permission", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Permission_PermissionType_PermissionTypeId",
+                        column: x => x.PermissionTypeId,
+                        principalSchema: "dbo",
+                        principalTable: "PermissionType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Permission_PermissionTypeId",
+                schema: "dbo",
+                table: "Permission",
+                column: "PermissionTypeId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Permission",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "PermissionType",
+                schema: "dbo");
+        }
+    }
+}
