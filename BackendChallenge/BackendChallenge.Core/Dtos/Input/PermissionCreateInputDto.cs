9f69f6ef8d0a18551c1596c5b6db37c8f63e792a
lmarroquindev <luismarroquindev@gmail.com>
2024-06-04 22:28:26 -0600

    Input dto for creation and update entities created

---
+++ BackendChallenge/BackendChallenge.Core/Dtos/Input/PermissionCreateInputDto.cs
@@ -0,0 +1,22 @@
+﻿using BackendChallenge.Core.Entities;
+
+namespace BackendChallenge.Core.Dtos.Input
+{
+    /// <summary>
+    /// DTO that contains the input data for creating a new record for the <see cref=“Permission”/> entity.
+    /// </summary>
+    public class PermissionCreateInputDto
+    {
+        /// <inheritdoc cref="Permission.FirstName"/>
+        public string FirstName { get; set; }
+
+        /// <inheritdoc cref="Permission.LastName"/>
+        public string LastName { get; set; }
+
+        /// <inheritdoc cref="Permission.PermissionTypeId"/>
+        public int PermissionTypeId { get; set; }
+
+        /// <inheritdoc cref="Permission.PermissionDate"/>
+        public DateTime PermissionDate { get; set; }
+    }
+}
---
+++ BackendChallenge/BackendChallenge.Core/Dtos/Input/PermissionUpdateInputDto.cs
@@ -0,0 +1,25 @@
+﻿using BackendChallenge.Core.Entities;
+
+namespace BackendChallenge.Core.Dtos.Input
+{
+    /// <summary>
+    /// DTO that contains the input data for updating a new record for the <see cref=“Permission”/> entity.
+    /// </summary>
+    public class PermissionUpdateInputDto
+    {
+        /// <inheritdoc cref="Permission.Id"/>
+        public int Id { get; set; }
+
+        /// <inheritdoc cref="Permission.FirstName"/>
+        public string FirstName { get; set; }
+
+        /// <inheritdoc cref="Permission.LastName"/>
+        public string LastName { get; set; }
+
+        /// <inheritdoc cref="Permission.PermissionTypeId"/>
+        public int PermissionTypeId { get; set; }
+
+        /// <inheritdoc cref="Permission.PermissionDate"/>
+        public DateTime PermissionDate { get; set; }
+    }
+}
