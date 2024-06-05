9cfb1c9ecdd3e50417784b51d0d5e8d316097bb6
lmarroquindev <luismarroquindev@gmail.com>
2024-06-04 21:08:28 -0600

    Entity framework mapping class by entities created

---
+++ BackendChallenge/BackendChallenge.Data/Mapping/PermissionMap.cs
@@ -0,0 +1,27 @@
+﻿using BackendChallenge.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BackendChallenge.Data.Mapping
+{
+    public class PermissionMap : IEntityTypeConfiguration<Permission>
+    {
+        public void Configure(EntityTypeBuilder<Permission> builder)
+        {
+            //Table
+            builder.ToTable("Permission", "dbo");
+
+            //PK
+            builder.HasKey(x => x.Id);
+
+            //Properties
+            builder.Property(x => x.FirstName).HasMaxLength(200).IsRequired();
+            builder.Property(x => x.LastName).HasMaxLength(200).IsRequired();
+            builder.Property(x => x.PermissionTypeId).IsRequired();
+            builder.Property(x => x.PermissionDate).IsRequired();
+
+            //Relationships
+            builder.HasOne(x => x.PermissionType).WithOne().HasForeignKey<Permission>(y => y.PermissionTypeId).OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ BackendChallenge/BackendChallenge.Data/Mapping/PermissionTypeMap.cs
@@ -0,0 +1,21 @@
+﻿using BackendChallenge.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BackendChallenge.Data.Mapping
+{
+    public class PermissionTypeMap : IEntityTypeConfiguration<PermissionType>
+    {
+        public void Configure(EntityTypeBuilder<PermissionType> builder)
+        {
+            //Table
+            builder.ToTable("PermissionType", "dbo");
+
+            //PK
+            builder.HasKey(x => x.Id);
+
+            //Properties
+            builder.Property(x => x.Description).HasMaxLength(300).IsRequired();
+        }
+    }
+}
