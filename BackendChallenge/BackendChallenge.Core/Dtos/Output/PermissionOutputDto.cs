718fe6719ef8da9ec864e96be83fbcb7624f5f24
lmarroquindev <luismarroquindev@gmail.com>
2024-06-04 20:57:42 -0600

    Entities and Output Dto Created

---
+++ BackendChallenge/BackendChallenge.Core/Dtos/Output/PermissionOutputDto.cs
@@ -0,0 +1,30 @@
+﻿using BackendChallenge.Core.Entities;
+
+namespace BackendChallenge.Core.Dtos.Output
+{
+    /// <summary>
+    /// Output Dto that represent to <see cref="Permission"/> entity.
+    /// </summary>
+    public class PermissionOutputDto
+    {
+        /// <inheritdoc cref="Permission.Id"/>
+        public int Id { get; set; }
+
+        /// <inheritdoc cref="Permission.FirstName"/>
+        public string FirstName { get; set; }
+
+        /// <inheritdoc cref="Permission.LastName"/>
+        public string LastName { get; set; }
+
+        /// <inheritdoc cref="Permission.PermissionTypeId"/>
+        public int PermissionTypeId { get; set; }
+
+        /// <inheritdoc cref="Permission.PermissionDate"/>
+        public DateTime PermissionDate { get; set; }
+
+        /// <summary>
+        /// Permission Type assigned to this Permission.
+        /// </summary>
+        public PermissionTypeOutputDto PermissionType { get; set; }
+    }
+}
