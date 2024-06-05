3149fd0e4b16fd1043dbcee912a077b682785da4
lmarroquindev <luismarroquindev@gmail.com>
2024-06-05 03:45:32 -0600

    List Permisons coponent created

---
+++ clientapp/src/components/Permission/ListaPermisos.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="container mt-5">
+    <h2 class="text-center mb-4">Permission List</h2>
+    <div class="row justify-content-center">
+      <div class="col-auto">
+        <table class="table table-bordered table-hover table-striped">
+          <thead class="thead-dark">
+            <tr>
+              <th>ID</th>
+              <th>First name</th>
+              <th>Last name</th>
+              <th>Permisson Type</th>
+              <th>Permisson Date</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="permiso in permisos" :key="permiso.id">
+              <td>{{ permiso.id }}</td>
+              <td>{{ permiso.firstNameEmployee }}</td>
+              <td>{{ permiso.lastNameEmployee }}</td>
+              <td>{{ permiso.permissionType.description }}</td>
+              <td>{{ formatIsoDate(permiso.permissionDate) }}</td>
+              <td>
+                <button
+                  class="btn btn-danger"
+                  @click="eliminarPermiso(permiso.id)"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+@import "bootstrap/dist/css/bootstrap.min.css";
+</style>
+
+<script>
+import { useFormatDate } from "@/composables/useFormatDate";
+import { useFetchData } from "@/composables/useFetchData";
+
+export default {
+  name: "ListaPermisos",
+  data() {
+    return {
+      permisos: [],
+      error: null,
+    };
+  },
+  methods: {
+    async eliminarPermiso(id) {
+      const { fetchData } = useFetchData(`Permission/${id}`, "DELETE");
+      await fetchData();
+
+      await this.fetchPermisos();
+    },
+    async fetchPermisos() {
+      const { data, fetchData } = useFetchData("Permission");
+
+      await fetchData();
+
+      if (data.value) {
+        this.permisos = data.value;
+      }
+    },
+  },
+  setup() {
+    const { formatIsoDate } = useFormatDate();
+    return {
+      formatIsoDate,
+    };
+  },
+  mounted() {
+    this.fetchPermisos();
+  },
+};
+</script>
