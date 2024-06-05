ab2adfbaf3ba9337bcb12e7478a1253a5d4e60d4
lmarroquindev <luismarroquindev@gmail.com>
2024-06-04 22:28:00 -0600

    Automapper profiles created to entities
    Mapping configuration between entities classes and dtos created

---
+++ BackendChallenge/BackendChallenge.Core/Automapper/PermissionProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using BackendChallenge.Core.Dtos.Input;
+using BackendChallenge.Core.Dtos.Output;
+using BackendChallenge.Core.Entities;
+
+namespace BackendChallenge.Core.Automapper
+{
+    public class PermissionProfile : Profile
+    {
+        public PermissionProfile()
+        {
+            CreateMap<Permission, PermissionOutputDto>();
+            CreateMap<PermissionCreateInputDto, Permission>();
+            CreateMap<PermissionUpdateInputDto, Permission>();
+        }
+    }
+}
