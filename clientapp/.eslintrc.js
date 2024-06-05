1c1300d79f6450b36485818b8343d062441d1102
lmarroquindev <luismarroquindev@gmail.com>
2024-06-05 03:41:49 -0600

    configuration to client proyect aplied

--- clientapp/.eslintrc.js
+++ clientapp/.eslintrc.js
@@ -2,17 +2,24 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    browser: true,
+    es2021: true,
   },
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "plugin:prettier/recommended",
+    "prettier",
   ],
   parserOptions: {
     parser: "@babel/eslint-parser",
+    ecmaVersion: 12,
+    sourceType: "module",
   },
+  plugins: ["vue"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": ["error", { endOfLine: "lf" }],
   },
 };
